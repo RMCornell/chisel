48b0cd73c84fec9e5f070813e9cd5be603e35509
R.M. Cornell <zen.alchemist5280@gmail.com>
2015-04-21 09:50:17 -0600

    Convert Headers (1-5) Conditional working

--- test/converter_test.rb
+++ test/converter_test.rb
@@ -10,25 +10,33 @@ class ConverterTest < MiniTest::Test
 	def test_converts_h1_tags
 		converter = Converter.new("# My Life in Desserts")
 		assert_equal "<h1>My Life in Desserts</h1>\n", converter.h1_tags
+		assert_equal "<h1>My Life in Desserts</h1>\n", converter.convert_headlines
 	end
 
 	def test_converts_h2_tags
 		converter = Converter.new("## My Life in Desserts")
 		assert_equal "<h2>My Life in Desserts</h2>\n", converter.h2_tags
+		assert_equal "<h2>My Life in Desserts</h2>\n", converter.convert_headlines
 	end
 
 	def test_converts_h3_tags
 		converter = Converter.new("### My Life in Desserts")
 		assert_equal "<h3>My Life in Desserts</h3>\n", converter.h3_tags
+		assert_equal "<h3>My Life in Desserts</h3>\n", converter.convert_headlines
 	end
 
 	def test_converts_h4_tags
 		converter = Converter.new("#### My Life in Desserts")
 		assert_equal "<h4>My Life in Desserts</h4>\n", converter.h4_tags
+		assert_equal "<h4>My Life in Desserts</h4>\n", converter.convert_headlines
 	end
 
 	def test_converts_h5_tags
 		converter = Converter.new("##### My Life in Desserts")
 		assert_equal "<h5>My Life in Desserts</h5>\n", converter.h5_tags
+		assert_equal "<h5>My Life in Desserts</h5>\n", converter.convert_headlines
 	end
+
+
+
 end
