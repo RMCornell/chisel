cbc45def22a5df9dc7a06982c97c409c7d58237f
kurohyou108 <robert.cornell78@gmail.com>
2015-04-21 18:56:06 -0600

    Redux_one

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,14 +1,48 @@
+require 'pry'
 require_relative 'parser'
 require_relative 'converter'
 
 class Chisel < Converter
-	attr_accessor :message
+
 	def initialize(message)
 		@message = message
+
 	end
 end
 
-message = File.read(ARGV[0])
-chisel = Chisel.new(message)
-puts chisel.h2_tags
+message = File.readlines(ARGV[0])
+converter = Converter.new(message)
+converter.convert_headlines
 
+
+
+=begin
+require './lib/header_converter'
+
+class Chisel < HeaderConverter
+  def initialize(text)
+    @text             = File.readlines(ARGV[0])
+    puts HeaderConverter.new(@text).convert_headers
+  end
+end
+
+puts Chisel.new(@header_converter)
+
+def convert
+  @text.each do |line|
+    if line.start_with?('#')
+      HeaderConverter.new(line).convert_headers
+    elsif line.start_with?('*')
+      line.SymbolConverter.new(line).convert_symbols
+end
+
+# something like this ^^^
+# basically once we have them all split up into classes
+# we need to create a new instance of that class in this conditional
+# to call the appropriate methods
+# if we call convert_headers for example becuase the line starts with '#'
+# the convert_headers method will then sort out which headers tags to apply
+# also Jeff Gu said that itll be better if use 'readlines' instead of
+# 'read' because we can iterate over each line and apply the logic above
+
+=end
