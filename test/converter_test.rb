6e984c463ecd5b153aaacbb5a205ffb35a959f4e
R.M. Cornell <zen.alchemist5280@gmail.com>
2015-04-21 19:28:45 -0600

    Started Converter - Git Test

---
+++ test/converter_test.rb
@@ -0,0 +1,38 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/converter'
+
+class ConverterTest < MiniTest::Test
+	def test_converter_exists
+		assert Converter.new("It Exists")
+	end
+end
+
+class HeadlineConverterTest < MiniTest::Test
+	def test_headline_converter_exists
+		assert HeadlineConverter.new("It Exists")
+	end
+
+	def test_it_converts_h1_tags
+		headline = HeadlineConverter.new("#Testing H1 Tags")
+		assert_equal "<h1>Testing H1 Tags</h1>", headline.h1_tag
+	end
+end
+
+class EmphasisConverterTest < MiniTest::Test
+	def test_emphasis_converter_exists
+		assert EmphasisConverter.new("It Exists")
+	end
+end
+
+class StrongConverterTest < MiniTest::Test
+	def test_strong_converter_exists
+		assert StrongConverter.new("It Exists")
+	end
+end
+
+class ParagraphTagTest < MiniTest::Test
+	def test_paragraph_tag_exists
+		assert ParagraphTag.new("It Exists")
+	end
+end
